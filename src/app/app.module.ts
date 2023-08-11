152aaab9f2a9ad4bea44db6349c56c8b58d1c3e1
unknown <senai@DSN1191109175.edu119.local>
2023-08-11 10:40:25 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FixedNoteComponent } from './components/fixed-note/fixed-note.component';
+import { FixedTabComponent } from './components/fixed-tab/fixed-tab.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FixedNoteComponent,
+    FixedTabComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
