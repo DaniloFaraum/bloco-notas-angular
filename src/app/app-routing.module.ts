55fb4353a012f913dc2aef5f4caa3d8c5e5ff0cb
unknown <senai@DSN1191109175.edu119.local>
2023-08-18 11:06:40 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FixedNoteComponent } from './components/fixed-note/fixed-note.component';
+import { NewNoteComponent } from './components/new-note/new-note.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: FixedNoteComponent
+  },
+  {
+    path: "create-note",
+    component: NewNoteComponent
+  },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
